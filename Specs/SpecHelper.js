d2facc94eae980d7924f7a6048204542e1f7068a
Ollie Riches <ollie.riches@btinternet.com>
2012-03-24 14:41:19 +0000

    Added jasmine dependancy and stubbed in spec runner

---
+++ Specs/SpecHelper.js
@@ -0,0 +1,9 @@
+﻿beforeEach(function () {
+    this.addMatchers({
+        toBePlaying: function (expectedSong) {
+            var player = this.actual;
+            return player.currentlyPlayingSong === expectedSong &&
+             player.isPlaying;
+        }
+    });
+});
