56125bdfac7c41256876794459fef9571e7aa970
Ollie Riches <ollie.riches@btinternet.com>
2012-03-24 14:47:36 +0000

    Removed jasmine files

--- Specs/SpecHelper.js
+++
@@ -1,9 +0,0 @@
-﻿beforeEach(function () {
-    this.addMatchers({
-        toBePlaying: function (expectedSong) {
-            var player = this.actual;
-            return player.currentlyPlayingSong === expectedSong &&
-             player.isPlaying;
-        }
-    });
-});
